a3b6b59035173014a1cd4dc508885de67f9c3016
primaam <pmaharyono@gmail.com>
2024-10-23 07:40:10 +0700

    fix hashing form

--- frontend/src/features/Hashing/Hashing.jsx
+++ frontend/src/features/Hashing/Hashing.jsx
@@ -2,24 +2,77 @@ import React, { useState } from "react";
 import { sha256 } from "js-sha256";
 import "./Hashing.css";
 
-const Hashing = () => {
-    const [inputText, setInputText] = useState("");
+const initialFormData = {
+    date: "",
+    name: "",
+    sex: "Male",
+    notes: "",
+};
 
-    const handleHash = () => {
-        console.log(sha256(inputText));
+const Hashing = () => {
+    const [formData, setFormData] = useState(initialFormData);
+
+    const handleChangeValue = (e) => {
+        const { name, value } = e.target;
+
+        setFormData({ ...formData, [name]: value });
     };
+
+    const handleHash = (e) => {
+        e.preventDefault();
+        if (!formData.name || !formData.date || !formData.notes || !formData.sex) {
+            return console.log("wrong input");
+        }
+        try {
+            const str = JSON.stringify(formData);
+            console.log(sha256(str));
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setFormData(initialFormData);
+        }
+    };
+
     return (
         <div id="sub-container">
-            <div id="hash-card">
+            <form id="form" onSubmit={handleHash}>
                 <h3>Hashing Card</h3>
+                <input name="date" type="date" value={formData.date} onChange={handleChangeValue} />
                 <input
-                    name="text"
-                    placeholder="Input your word to hash"
-                    value={inputText}
-                    onChange={(e) => setInputText(e.target.value)}
+                    name="name"
+                    placeholder="Input your name"
+                    value={formData.name}
+                    onChange={handleChangeValue}
                 />
-                <button onClick={handleHash}>Submit</button>
-            </div>
+                <div className="hashing-section">
+                    <label>Choose your gender</label>
+                    <div id="form-radio">
+                        <input
+                            type="radio"
+                            name="sex"
+                            value={"Male"}
+                            onChange={handleChangeValue}
+                        />
+                        <label>Male</label>
+                    </div>
+                    <div id="form-radio">
+                        <input
+                            type="radio"
+                            name="sex"
+                            value={"Female"}
+                            onChange={handleChangeValue}
+                        />
+                        <label>Female</label>
+                    </div>
+                </div>
+                <input
+                    name="notes"
+                    placeholder="Input your notes"
+                    value={formData.notes}
+                    onChange={handleChangeValue}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 };
